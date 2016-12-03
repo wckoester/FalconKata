b48e5cd9023b6fcb00d2470813d3b3690469c6ee
wckoester <wckoester@gmail.com>
2016-12-02 20:17:51 -0800

    multiple test case categories, setup for make dist tarball generation

--- tests/check_roman.c
+++ tests/check_roman.c
@@ -7,18 +7,27 @@ START_TEST(always_fail)
    ck_assert_int_eq(3,4);
 END_TEST
 
+START_TEST(always_pass)
+   ck_assert_int_eq(2,3);
+END_TEST
+
 static Suite *roman_suite(void)
 {
    Suite *s;
    TCase *tc_core;
+   TCase *tc_limits;
 
    s = suite_create("Roman");
    
    tc_core = tcase_create("Core");
 
    tcase_add_test(tc_core, always_fail);
-
    suite_add_tcase(s, tc_core);
+
+   tc_limits = tcase_create("Limits");
+
+   tcase_add_test(tc_limits, always_pass);
+   suite_add_tcase(s, tc_limits);
 
    return s;
 }
